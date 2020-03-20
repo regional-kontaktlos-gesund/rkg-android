693e1493370609d9aa75cadd3cd6d7c2293e4d9f
PhilippNowak96 <dev@philipp-nowak.com>
2020-03-21 00:05:59 +0100

    Initial commit

---
+++ app/src/main/java/org/wirvsvirus/rkg/MainActivity.kt
@@ -0,0 +1,18 @@
+package org.wirvsvirus.rkg
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.wirvsvirus.rkg.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/org/wirvsvirus/rkg/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package org.wirvsvirus.rkg.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
