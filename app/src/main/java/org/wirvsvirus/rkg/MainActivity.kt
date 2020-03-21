2be714cef7bf46bef57cadc7eceb82bcca312312
langerhans <langerhans@users.noreply.github.com>
2020-03-21 16:35:00 +0100

    Navigation

--- app/src/main/java/org/wirvsvirus/rkg/MainActivity.kt
+++ app/src/main/java/org/wirvsvirus/rkg/MainActivity.kt
@@ -2,6 +2,9 @@ package org.wirvsvirus.rkg
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.main_activity.*
+import org.wirvsvirus.rkg.ui.SortimentFragment
 import org.wirvsvirus.rkg.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +12,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            val dest = when (it.itemId) {
+                R.id.navSortiment -> R.id.sortimentFragment
+                // ...
+                else -> -1
+            }
+
+            if (dest > -1) {
+                findNavController(R.id.navHost).navigate(dest)
+            }
+
+            return@setOnNavigationItemSelectedListener true
         }
     }
 }
