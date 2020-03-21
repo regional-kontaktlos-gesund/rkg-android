94f0c7a795dfe25424dbd15e371f26500c585ecc
langerhans <langerhans@users.noreply.github.com>
2020-03-21 01:00:27 +0100

    Add map boilerplate

--- app/src/main/java/org/wirvsvirus/rkg/ui/main/MainFragment.kt
+++ app/src/main/java/org/wirvsvirus/rkg/ui/main/MainFragment.kt
@@ -1,11 +1,13 @@
 package org.wirvsvirus.rkg.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.main_fragment.*
 import org.wirvsvirus.rkg.R
 
 class MainFragment : Fragment() {
@@ -25,6 +27,46 @@ class MainFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+
+        mainMap.onCreate(savedInstanceState)
+        mainMap.getMapAsync { map ->
+            Log.d("main", "map is ready!")
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mainMap.onResume()
+    }
+
+    override fun onPause() {
+        mainMap.onPause()
+        super.onPause()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mainMap.onStart()
+    }
+
+    override fun onStop() {
+        mainMap.onStop()
+        super.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        mainMap.onSaveInstanceState(outState)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        mainMap?.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        mainMap.onLowMemory()
+        super.onLowMemory()
     }
 
 }
